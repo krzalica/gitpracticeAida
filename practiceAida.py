9bd887923af11492b07fdf7260e0961b5ab69ec9
kimmypracha <pracha.promtaow@gmail.com>
2020-02-25 11:34:50 -0800

    added partner 2

--- practiceAida.py
+++ practiceAida.py
@@ -1,4 +1,7 @@
 #Partner 1: Aida
+#Partner 2: Kim
+#########################
+
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     pass
